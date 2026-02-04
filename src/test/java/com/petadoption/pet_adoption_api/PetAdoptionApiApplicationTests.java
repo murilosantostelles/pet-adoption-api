691d7a911bbd7a51b10b16e47795bcd92af8a5ff
Murilo Santos Telles <murilosantos.work@gmail.com>
2026-02-04 18:58:46 -0300

    refactor: reorganiação de estrutura de pastas

---
+++ src/test/java/com/petadoption/pet_adoption_api/PetAdoptionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petadoption.pet_adoption_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetAdoptionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
