ad22f7832625d2404b445e8ed1eaf427a8276cd2
Murilo Santos Telles <murilosantos.work@gmail.com>
2026-02-27 20:01:54 -0300

    feat: Testes Unitários de FindById e DeletePet implementados

--- src/test/java/com/petadoption/pet_adoption_api/service/PetServiceTest.java
+++ src/test/java/com/petadoption/pet_adoption_api/service/PetServiceTest.java
@@ -11,6 +11,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import static org.mockito.ArgumentMatchers.any;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -54,5 +59,53 @@ public class PetServiceTest {
         assertEquals(5.5, result.getWeight());
         assertEquals("Vira-lata", result.getRace());
         verify(petRepository, times(1)).save(any(Pet.class));
+    }
+
+    @Test
+    @DisplayName("Deve encontrar todos os pets cadastrados")
+    void testFindAllPets(){
+        List<Pet> pets = List.of(pet);
+        when(petRepository.findAll()).thenReturn(pets);
+
+        List<Pet> result = petService.findAllPets();
+
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        verify(petRepository, times(1)).findAll();
+    }
+
+    @Test
+    @DisplayName("Deve buscar pet por id com sucesso")
+    void testFindByIdSuccess(){
+        when(petRepository.findById(2L)).thenReturn(Optional.of(pet));
+
+        Pet result = petService.findById(2L);
+
+        assertNotNull(result);
+        assertEquals(2L, result.getId());
+    }
+
+    @Test
+    @DisplayName("Deve lançar uma excessão ao buscar por um pet inexistente")
+    void testFindByIdNotFound(){
+        when(petRepository.findById(999L)).thenReturn(Optional.empty());
+        assertThrows(RuntimeException.class, () -> petService.findById(999L));
+    }
+
+    @Test
+    @DisplayName("Deve deletar pet com sucesso")
+    void testDeletePet(){
+        when(petRepository.existsById(2L)).thenReturn(true);
+
+        petService.deletePet(2L);
+        verify(petRepository, times(1)).deleteById(2L);
+    }
+
+    @Test
+    @DisplayName("Deve lançar excessão ao tentar deletar pet inexistente")
+    void testDeletePetNotFound(){
+        when(petRepository.existsById(999L)).thenReturn(false);
+
+        assertThrows(RuntimeException.class, () -> petService.deletePet(999L));
     }
 }
