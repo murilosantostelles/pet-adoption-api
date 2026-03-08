0a2983945b588082ac49e9d19438cf5603a856a3
Murilo Santos Telles <murilosantos.work@gmail.com>
2026-03-08 19:49:07 -0300

    refactor: otimizar uso de memória em campos de texto

--- src/main/java/com/petadoption/pet_adoption_api/entity/Address.java
+++ src/main/java/com/petadoption/pet_adoption_api/entity/Address.java
@@ -1,5 +1,6 @@
 package com.petadoption.pet_adoption_api.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,8 +13,13 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Address {
+
+    @Column(length = 100)
     private String street;
+
     private Integer number;
+
+    @Column(length = 60)
     private String city;
 
 }
